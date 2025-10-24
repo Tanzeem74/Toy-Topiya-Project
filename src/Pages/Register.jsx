b06145b7fa4ff3b35103a5bf66d46cf9171d2f12
Tanzeem <tanzeemafsar74@gmail.com>
2025-10-25 01:12:48 +0600

    toggle password added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
+import { IoEye, IoEyeOff } from 'react-icons/io5';
 import { Link, } from 'react-router';
 
 const Register = () => {
     useEffect(() => {
         document.title = "Register";
     }, []);
+    const [show, setShow] = useState(false);
+    const handleTogglePass=(e)=>{
+        e.preventDefault();
+        setShow(!show);
+    }
     return (
         <div className='flex justify-center min-h-screen items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
@@ -21,8 +27,17 @@ const Register = () => {
                         <label className="label">Email</label>
                         <input name='email' type="email" className="input" placeholder="Email" required />
 
-                        <label className="label">Password</label>
-                        <input name='password' type="password" className="input" placeholder="Password" required />
+                        <div className='relative'>
+                            <label className="label">Password</label>
+                            <input name='password'
+                                type={show ? 'text' : 'password'}
+                                className="input" placeholder="Password" required />
+                            <button
+                                onClick={handleTogglePass}
+                                className='btn btn-sm absolute top-6 right-5'>
+                                    {show?<IoEyeOff />:<IoEye />}
+                                </button>
+                        </div>
 
 
                         <button type='submit' className="btn btn-neutral mt-4">Register</button>
